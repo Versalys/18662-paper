d45ce530d6d20d90068dade7dac22a1b6963c9bf
Versalys <50535106+Versalys@users.noreply.github.com>
2024-03-31 21:05:44 -0400

    Update for midterm submission

--- hids_model.py
+++ hids_model.py
@@ -5,22 +5,19 @@ from torch.nn import functional as F
 # TODO: Look at hidden states. Update lstm to not pass tuple. Use DL homework.
 
 class HidsModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers=50):
         super().__init__()
 
-        self.mods = nn.Sequential(nn.LSTM(input_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.LSTM(hidden_dim, hidden_dim),
-                                     nn.Linear(hidden_dim, output_dim),
-                                     nn.Sigmoid()
-                                     )
+        self.l_mods = nn.Sequential(
+            nn.LSTM(input_dim, hidden_dim, num_layers=num_layers, batch_first=True)
+        )
+
+        self.linear = nn.Linear(hidden_dim, output_dim)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.mods(x)
+        for mod in self.l_mods:
+            x, _ = mod(x)
+        x = self.linear(x)
+        x = self.sigmoid(x)
         return x
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from hids_model import HidsModel
 from os import listdir
+from tqdm import tqdm
 
 
 NORMAL_ROOT = 'data/ADFA-LD/Training_Data_Master/'
 ATTACK_ROOT = 'data/ADFA-LD/Attack_Data_Master/'
 ATTACKS = ['AddUser', 'Hydra_FTP', 'Hydra_SSH', 'Java_Meterpreter', 'Meterpreter', 'Web_Shell']
 NUM_FEATURES = 341
-
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") # GPU acceleration
 
 def assemble_attack_data(arch_root=ATTACK_ROOT):
     data = {}
@@ -47,35 +48,70 @@ def create_dataloader(attack_data, normal_data, excl=set()):
 
 
 def make_one_hot(n, num_classes):
-    print(n)
     tensor = F.one_hot(n, num_classes)
-    return tensor.float()
+    return torch.unsqueeze(tensor.float(), dim=0).to(DEVICE)
+
+
+def save_model(model):
+    torch.save(model, './model.pt')
+
+def load_model():
+    model = torch.load('./model.pt')
+    return model
 
 
 def train_model(dataloader, n_epochs):
-    model = HidsModel(NUM_FEATURES, 128, 1)
+    print(f"Training with device: '{DEVICE}'")
+    model = HidsModel(NUM_FEATURES, 256, 1)
     criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters())
     for epoch in range(n_epochs):
         cumulative_loss = 0
-        for x, y in dataloader:
+        max_loss = -float('inf')
+        min_loss = float('inf')
+        for x, y in tqdm(dataloader, desc=f'Epoch [{epoch}]'):
             optimizer.zero_grad()
             hot_x = make_one_hot(torch.LongTensor(x), NUM_FEATURES)
-            print(hot_x)
-            output = model(hot_x)
-            loss = criterion(output, y)
+            # print(hot_x.shape)
+            output = model(hot_x)[0,-1]
+            # print(output.shape)
+            # print('===', output)
+            loss = criterion(output, y.float())
             loss.backward()
             optimizer.step()
 
             cumulative_loss += loss.item()
-        print(f'Epoch [{epoch}] : {cumulative_loss}')
+            max_loss = max(max_loss, loss.item())
+            min_loss = min(min_loss, loss.item())
+        cumulative_loss /= len(dataloader)
+        print(f'Epoch [{epoch}] : <{cumulative_loss}> | Max Loss: {max_loss} | Min Loss: {min_loss}')
+    return model
+
+
+def test_model(model, dataloader):
+    model.eval()
+    correct = 0
+    for x, y in tqdm(dataloader, desc='<Testing model>'):
+        hot_x = make_one_hot(torch.LongTensor(x), NUM_FEATURES)
+        output = model(hot_x)[-1]
+        choice = 0 if output <= .5 else 1
+        if choice == y:
+            correct += 1
+    accuracy = correct / len(dataloader)
+    print(f"Accuracy: {round(accuracy*100, 3)}%")
 
 
 def main():
     attack = assemble_attack_data()
     normal = assemble_normal_data()
     dataloader = create_dataloader(attack, normal)
-    train_model(dataloader, 15)
+
+    model = train_model(dataloader, 5)
+    save_model(model)
+
+    # model = load_model()
+
+    test_model(model, dataloader)
 
 
 if __name__ == '__main__':
